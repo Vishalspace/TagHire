ef36e02da944b9cb1957433d1556bca196525633
VishalY <vishaly.space@gmail.com>
2022-05-02 19:44:59 +0530

    dashed rounder corners

--- app/src/main/java/com/vishal/taghire/MainActivity.kt
+++ app/src/main/java/com/vishal/taghire/MainActivity.kt
@@ -4,15 +4,16 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.vishal.taghire.api.WazirApi
 import com.vishal.taghire.databinding.ActivityMainBinding
 import com.vishal.taghire.ui.QuotesAdapter
 import com.vishal.taghire.utils.addTo
 import com.vishal.taghire.utils.convertLongToTime
+import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
@@ -20,7 +21,6 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var api: WazirApi
     private lateinit var binding: ActivityMainBinding
-    private lateinit var swipeRefresh: SwipeRefreshLayout
     private val compositeDisposable = CompositeDisposable()
     private val adapter = QuotesAdapter()
 
@@ -30,32 +30,37 @@ class MainActivity : AppCompatActivity() {
         (application as App).component.inject(this)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         initUi(binding)
-        callApi()
     }
 
     private fun initUi(binding: ActivityMainBinding) {
-        swipeRefresh = binding.swipeRefreshLayout
-        binding.tvat.text = "Fetching data"
+        binding.tvat.text = "Fetching data..."
         binding.currencyrecycler.layoutManager = LinearLayoutManager(this)
         binding.currencyrecycler.adapter = adapter
+        binding.swipeRefreshLayout.setOnRefreshListener {
+            callApi()
+        }
     }
 
     override fun onResume() {
         super.onResume()
-        refresh()
+        startAutoRefresh()
     }
 
-    private fun refresh() {
-        swipeRefresh.setOnRefreshListener {
-            callApi()
-        }
+    private fun startAutoRefresh() {
+        Observable.interval(0, 4, TimeUnit.SECONDS, Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnEach {
+                if (!binding.swipeRefreshLayout.isRefreshing) callApi()
+            }
+            .subscribe()
+            .addTo(compositeDisposable)
     }
 
     private fun callApi() {
         api.getQuotes()
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
-            .doAfterTerminate { swipeRefresh.isRefreshing = false }
+            .doAfterTerminate { binding.swipeRefreshLayout.isRefreshing = false }
             .subscribe { result ->
                 adapter.setData(result)
                 binding.tvat.text = convertLongToTime(result.first().at)
